5bbb72dc3f02a578a6013b2676ab770678622ead
ituitis-ak19 <bilalak1907@gmail.com>
2022-08-27 23:31:47 +0300

    f

--- vhs_application/lib/main.dart
+++ vhs_application/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:vhs_application/core/widgets/buttons/camera_buttonS.dart';
+import 'package:vhs_application/screen/folder/view/folder_view.dart';
 import 'package:vhs_application/screen/home/view/home_view.dart';
 import 'package:vhs_application/screen/tutorial/view/tutorial_view_2.dart';
 import 'package:vhs_application/screen/tutorial/view/tutorial_view_3.dart';
@@ -23,7 +24,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(fontFamily: 'Alpharush'),
       title: 'Material App',
-      home: TutorialView3()
+      home: FolderView()
     );
   }
 }
